95560f2f0c7fe80f17c94531946feb905424c7b8
Harmeet Singh <128402172+hskhanduja03@users.noreply.github.com>
2024-08-14 15:20:15 +0530

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,7 +4,11 @@ function Home() {
   return (
     <div className='bg-gray-600 h-screen flex flex-col justify-center items-center text-white'>
       <h1>This is Home Page</h1>
-      <p className='border p-2 my-2'><span className='font-bold'>Note:  </span>Do not refresh after login as "token" might get deleted</p>
+      <p className='border p-2 my-2'><span className='font-bold'>Note:  </span>
+        1.Do not refresh after login as "token" might get deleted
+        <br/>
+        2. You might have to reload a few times as backend is on "RENDER - FREE Version"
+      </p>
     </div>
   )
 }
